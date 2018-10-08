755738238f6721213c28b4fc8ab3dbc62381aed4
Sumon Sarker <sumonsarker74@gmail.com>
2018-10-08 19:51:44 +0600

    Delete grant.sql

--- grant.sql
+++
@@ -1,7 +0,0 @@
-GRANT ALL ON db1.* TO 'username'@'hostname';
-
-GRANT SELECT ON db2.invoice TO 'username'@'hostname';
-
-GRANT SELECT, INSERT ON mydb.mytbl TO 'someuser'@'somehost';
-
-GRANT SELECT (col1), INSERT (col1,col2) ON mydb.mytbl TO 'someuser'@'somehost';
